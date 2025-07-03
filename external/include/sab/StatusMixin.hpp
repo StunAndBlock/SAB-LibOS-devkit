e7f213945e2bb4bd17ca8d2800ebd22891321c5d
StunAndBlock <dword.byteme@mail.ru>
2025-07-03 23:49:59 +0300

    Build and external folder are now completely ignored; External libraries are now referenced but not include in repo, download it yourself, changes will be reflected in readme later;

--- external/include/sab/StatusMixin.hpp
+++ external/include/sab/StatusMixin.hpp
@@ -2,22 +2,21 @@
 #define SAB_STATUS_MIXIN_HPP_
 
 namespace sab {
-    template <typename Derived>
+    template <typename StatusEnum>
     class StatusMixin {
         protected:
-            using StatusEnum = typename Derived::Status;
             StatusEnum status_;
         public:
+            StatusMixin() = default;
             virtual ~StatusMixin() = 0;  
             StatusEnum getStatus() const;
     };
 
-    template <typename Derived>
-    StatusMixin<Derived>::~StatusMixin() = default;
+    template <typename StatusEnum>
+    StatusMixin<StatusEnum>::~StatusMixin() = default;
 
-    template <typename Derived>
-    typename StatusMixin<Derived>::StatusEnum 
-    StatusMixin<Derived>::getStatus() const {
+    template <typename StatusEnum>
+    StatusEnum StatusMixin<StatusEnum>::getStatus() const {
         return status_;
     }
 
